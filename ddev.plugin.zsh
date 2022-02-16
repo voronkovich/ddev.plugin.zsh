280c39827145a7e89c37fa1b065395d17a4f35aa
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-02-16 22:22:51 +0300

    Fix DDEV detecting

--- ddev.plugin.zsh
+++ ddev.plugin.zsh
@@ -2,7 +2,7 @@ ZSH_PLUGIN_DDEV_TOOLS=(composer yarn npm ${ZSH_PLUGIN_DDEV_TOOLS})
 
 for tool in $ZSH_PLUGIN_DDEV_TOOLS; do
     "${tool}"() {
-        if [[ -d .ddev ]]; then
+        if [[ -f '.ddev/config.yaml' ]]; then
             ddev exec "${funcstack}" "$@"
         else
             "$(/usr/bin/which "${funcstack}")" "$@"
