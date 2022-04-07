e60bf6f3597b9076e2f3b425b0e407d2d5cad5fe
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-04-07 18:42:02 +0300

    Add wrapper for "ddev exec"

--- ddev.plugin.zsh
+++ ddev.plugin.zsh
@@ -1,14 +1,36 @@
-ZSH_PLUGIN_DDEV_TOOLS=(composer yarn npm ${ZSH_PLUGIN_DDEV_TOOLS})
+ZSH_PLUGIN_DDEV_TOOLS=(composer yarn npm npx ${ZSH_PLUGIN_DDEV_TOOLS})
 
 for tool in $ZSH_PLUGIN_DDEV_TOOLS; do
     "${tool}"() {
         if [[ -f '.ddev/config.yaml' ]]; then
-            ddev exec "${funcstack}" "$@"
+            ddev exec -- "${funcstack}" "$@"
         else
             "$(/usr/bin/which "${funcstack}")" "$@"
         fi
     }
 done
+
+dev() {
+    if [[ $# == 0 ]]; then
+        ddev ssh
+
+        return
+    fi
+
+    if [[ $1 =~ '^(-h|--help)$' ]]; then
+        cat <<HELP
+Wrapper for "ddev exec"
+
+Usage: dev [<command>] [<args>]
+
+Example: dev npm install
+HELP
+
+        return
+    fi
+
+    ddev exec -- "$@"
+}
 
 ddev-install() {
     curl https://raw.githubusercontent.com/drud/ddev/master/scripts/install_ddev.sh | bash
